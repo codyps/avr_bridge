cf2b3bca1ab56b1cb5bea622ba7dbb2ca1692e58
Cody Schafer <cpschafer@gmail.com>
2011-02-05 02:51:28 -0500

    gen: fix warning regarding shift beyond ype width

--- src/gen_avr_utils.py
+++ src/gen_avr_utils.py
@@ -150,7 +150,7 @@ def deserialize_primative(f, buffer_addr, field):
 		f.line('{0}.base = 0;'.format(this))
 		for byte in range(0, clen):
 			byte_i = clen - byte - 1
-			ol = '{0}.base |= *({1} + offset + {2}) << (8 * {3});' 
+			ol = '{0}.base |= (typeof({0}))*({1} + offset + {2}) << (8 * {3});' 
 			f.line(ol.format(this, buffer_addr, byte_i, byte))
 
 		f.line('this->{0} = {1}.real;'.format(fname, this))
