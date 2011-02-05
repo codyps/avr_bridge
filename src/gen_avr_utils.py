3b33e325209975b9013cf36104e8ecb4af010a8f
Cody Schafer <cpschafer@gmail.com>
2011-02-05 03:45:50 -0500

    gen: fix Ros ros instanciation.

--- src/gen_avr_utils.py
+++ src/gen_avr_utils.py
@@ -431,7 +431,7 @@ class CGenerator():
 		f.line('}')
 
 
-		f.line('Ros ros("{0}", {1});'.format(self.config['name'], len(self.topicIds)))
+		f.line('Ros <{1}> ros("{0}");'.format(self.config['name'], len(self.topicIds)))
 		f.close()
 		
 		
